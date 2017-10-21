78cbb1eedf932fdf9527a0c55b2ea37ac7868dc2
Kevin James <kevin@talkiq.com>
2017-10-20 19:22:10 -0700

    test(gcloud): add regression test for KeyError in process_log_record

--- tests/gcloud/log_test.py
+++ tests/gcloud/log_test.py
@@ -9,6 +9,12 @@ def test_get_handler():
     assert isinstance(handler.formatter, JsonFormatter)
 
 
+def test_no_levelname():
+    # should NOT throw a KeyError
+    JsonFormatter().process_log_record({'nolevelname': 'aardvark'})
+    assert True
+
+
 def test_formats_for_gcloud():
     from pythonjsonlogger import jsonlogger
     m = jsonlogger.JsonFormatter.process_log_record = mock.MagicMock()
