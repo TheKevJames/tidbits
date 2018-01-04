fd86d7343cffad741bd0185f6406feef4088befd
Kevin James <kevin@talkiq.com>
2018-01-03 17:22:28 -0800

    chore(release): release v0.3.1

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(os.path.join(PACKAGE_ROOT, 'requirements.txt')) as f:
 
 setuptools.setup(
     name='tidbits',
-    version='0.3.0',
+    version='0.3.1',
     description='Useful Python Tidbits',
     long_description=README,
     packages=setuptools.find_packages(exclude=('tests',)),
