e75ce6c1ab1d553dce4f940a34474248ce6d69b1
Kevin James <KevinJames@thekev.in>
2018-04-09 15:13:58 -0700

    fix(gcloud): fix py2 parsing for JsonFormatter init

--- tidbits/gcloud/log.py
+++ tidbits/gcloud/log.py
@@ -9,8 +9,8 @@ FMT = '%(filename)s %(lineno)d %(levelname)s %(message)s %(name)s'
 
 class JsonFormatter(jsonlogger.JsonFormatter):
     # pylint: disable=too-few-public-methods
-    def __init__(self, *args, fmt=FMT, **kwargs):
-        jsonlogger.JsonFormatter.__init__(self, *args, fmt=fmt, **kwargs)
+    def __init__(self, fmt=FMT, *args, **kwargs):
+        jsonlogger.JsonFormatter.__init__(self, fmt=fmt, *args, **kwargs)
 
     def process_log_record(self, log_record):
         # stackdriver uses "severity" instead of "levelname"
